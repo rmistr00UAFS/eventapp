48cac1b0849b79142f323f9404565e2b8a73f7f0
Ronak Mistry <rmistr00@uafs.edu>
2024-09-25 00:58:26 -0500

    beta branch

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,8 @@ import HelloWorld from './components/HelloWorld.vue'
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
-      test
+      testing
+      s
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
