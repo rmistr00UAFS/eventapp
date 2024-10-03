e755d53b277a07040f98950202d78c54222296f8
Ronak Mistry <rmistr00@uafs.edu>
2024-10-03 03:47:34 -0500

    added views and components in each view

---
+++ src/views/Test.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const test = ref({})
+
+const getData = () => {
+  fetch('http://localhost/getEvents.php')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      test.value = data
+
+      console.log(test)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+//get data
+getData()
+</script>
+
+<template>
+  TESTING AREA
+  <!--
+  <div v-for="user in test" :key="user.id" class="user">
+        {{ user.USERID }}
+      </div>-->
+</template>
+
+<style scoped></style>
