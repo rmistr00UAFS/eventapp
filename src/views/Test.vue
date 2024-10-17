6bc71cb0697eba5d009f2f2d46d8395ecd6fd080
Ronak Mistry <rmistr00@uafs.edu>
2024-10-16 23:41:08 -0500

    fin

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,7 +1,21 @@
-<script setup lang="ts"></script>
 
-<template>
-  <attenders :count="45" />
-</template>
 
-<style scoped></style>
+<script setup lang="ts">
+let testFunction1 = function () {
+  console.log('test1')
+}
+
+let testFunction2 = () => {
+  console.log('test2')
+}
+
+testFunction2()
+</script>
+
+<template><button class="test_button" @click="testFunction1">Test</button></template>
+
+<style scoped>
+.test_button {
+  background: red;
+}
+</style>
