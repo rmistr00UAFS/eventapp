9a314dc3ee5c7074a5a790d7dc9be02db42be335
DanielK08 <danieljarkiss@gmail.com>
2024-11-01 19:36:46 -0500

    ughhhhh

--- backend/a/functions/write.php
+++ backend/a/functions/write.php
@@ -1,5 +1,6 @@
 <?php
 
+
 function createReview($mysqli,$reviewID,$userID, $eventID, $comment, $stars){
     $stmt = $mysqli->prepare("INSERT INTO `REVIEWS` (`REVIEWID`, `USERID`, `EVENTID`, `COMMENT`, `STARS`) VALUES (?, ?, ?, ?, ?)");
     $stmt->bind_param("iiisi", $reviewID, $userID, $eventID, $comment, $stars);
